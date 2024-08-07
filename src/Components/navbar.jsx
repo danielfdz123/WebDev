da36a4dc1f37e1e7778a1aed8c4ed0b6e7d7195c
danielfdz123 <daniel.fernandez16@myhunter.cuny.edu>
2024-08-06 20:12:41 -0400

    added homepage structure

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -8,18 +8,18 @@ export function NavBar() {
   return (
     <>
       <div class = "NavBar">
-      <img src={logo} className="NavBarLogo" alt="Logo" />  
-        <Link to = "/">
-          <button class = "NavButtons"> Home </button>
-        </Link>
-        <Link to="/members">
-          <button class = "NavButtons"> Members </button>
-        </Link>
-        <Link to="/tasks">
-          <button class = "NavButtons"> Tasks </button>
-        </Link>
-      <Outlet />
-    </div> 
+        <img src={logo} className="NavBarLogo" alt="Logo" />  
+          <Link to = "/">
+            <button class = "NavButtons"> Home </button>
+          </Link>
+          <Link to="/members">
+            <button class = "NavButtons"> Members </button>
+          </Link>
+          <Link to="/tasks">
+            <button class = "NavButtons"> Tasks </button>
+          </Link>
+        <Outlet />
+      </div> 
     </>
   );
 }
